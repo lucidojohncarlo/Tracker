e1d3318c6793ee9cd5b0ef187192a804bad45256
lucidojm <lucidojohncarlo10@gmail.com>
2024-06-25 00:15:00 +0800

    6-24-24

--- db_connect.php
+++ db_connect.php
@@ -4,10 +4,10 @@
 //$password = ""; // Your database password
 //$dbname = "budget_tracker"; // Your database name
 
-$servername = "lucidojohncarlo.mysql.database.azure.com"; // Azure MySQL server hostname
-$username = "lucidojohncarlo"; // Azure MySQL username
-$password = "Jhared123"; // Azure MySQL password
-$dbname = "budget_tracker"; // Azure MySQL database name
+$servername = "lucidojohncarlo.mysql.database.azure.com";
+$username = "lucidojohncarlo";
+$password = "Jhared123";
+$dbname = "budget_tracker";
 $port = 3306;
 
 $ssl_cert_path = "/home/site/wwwroot/DigiCertGlobalRootCA.crt.pem";
